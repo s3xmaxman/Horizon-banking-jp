21561d42b162f62b7927b394181553cd7cf4cb9c
s3xmaxman <badside3919@gmail.com>
2024-05-15 01:00:42 +0900

    Auth Page UI

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,9 +2,34 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+
 
 const AuthForm = ({ type } : { type: string }) => {
   const [user, setUser] = useState(null)
+
+    // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+  })
+     
+      // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
   return (
     <section className='auth-form'>
         <header className='flex flex-col gap-5 md:gap-8'>
@@ -28,8 +53,23 @@ const AuthForm = ({ type } : { type: string }) => {
                 <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
                   {user ? 'Link Account' : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
                 </h1>
+                <p className='text-16 font-normal text-gray-600'>
+                {user 
+                  ? 'アカウントをリンクしてスタートしましょう'
+                  : '詳細を入力してください'
+                }
+                </p>
             </div>
         </header>
+        {user ? (
+            <div className='flex flex-col gap-4'>
+                {/* plaid link */}
+            </div>
+        ): (
+            <>
+            Form
+            </>
+        )}
     </section>
   )
 }
