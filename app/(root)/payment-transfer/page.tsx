da277210a2718aba409e5555a712aebd0a34d2e0
s3xmaxman <badside3919@gmail.com>
2024-05-17 08:28:22 +0900

    progress

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,18 @@
 import HeaderBox from '@/components/HeaderBox'
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Transfer= () => {
+const Transfer= async () => {
+  const loggedIn = await getLoggedInUser()
+  const accounts = await getAccounts({ userId: loggedIn.$id })
+  
+  if(!accounts) {
+    return null
+  }
+
+  const accountsData = accounts?.data
   return (
     <section className='payment-transfer'>
       <HeaderBox
@@ -9,7 +20,7 @@ const Transfer= () => {
         subtext="支払い振替に関する詳細やメモがあればご記入ください" 
       />
       <section className='size-full pt-5'>
-
+        <PaymentTransferForm accounts={accountsData} />
       </section>
     </section>
   )
--- components/PaymentTransferForm.tsx
+++ components/PaymentTransferForm.tsx
@@ -1,84 +1,254 @@
 "use client";
 
-import Image from "next/image";
-import { useSearchParams, useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
+import { createTransfer } from "@/lib/actions/dwolla.actions";
+// import { createTransaction } from "@/lib/actions/transaction.actions";
+import { getBank, getBankByAccountId } from "@/lib/actions/user.actions";
+import { decryptId } from "@/lib/utils";
+
+import { BankDropdown } from "./BankDropdown";
+import { Button } from "./ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-} from "@/components/ui/select";
-import { formUrlQuery, formatAmount } from "@/lib/utils";
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
-export const BankDropdown = ({
-  accounts = [],
-  setValue,
-  otherStyles,
-}: BankDropdownProps) => {
-  const searchParams = useSearchParams();
+const formSchema = z.object({
+    email: z.string().email("メールアドレスが無効です"),
+    name: z.string().min(4, "送金メモが短すぎます"),
+    amount: z.string().min(4, "金額が短すぎます"),
+    senderBank: z.string().min(4, "有効な銀行口座を選択してください"),
+    sharableId: z.string().min(8, "有効なShareable IDを選択してください"),
+});
+
+const PaymentTransferForm = ({ accounts }: PaymentTransferFormProps) => {
   const router = useRouter();
-  const [selected, setSeclected] = useState(accounts[0]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleBankChange = (id: string) => {
-    const account = accounts.find((account) => account.appwriteItemId === id)!;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      amount: "",
+      senderBank: "",
+      sharableId: "",
+    },
+  });
 
-    setSeclected(account);
-    const newUrl = formUrlQuery({
-      params: searchParams.toString(),
-      key: "id",
-      value: id,
-    });
-    router.push(newUrl, { scroll: false });
+  const submit = async (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
 
-    if (setValue) {
-      setValue("senderBank", id);
+    try {
+      const receiverAccountId = decryptId(data.sharableId);
+      const receiverBank = await getBankByAccountId({
+        accountId: receiverAccountId,
+      });
+      const senderBank = await getBank({ documentId: data.senderBank });
+
+      const transferParams = {
+        sourceFundingSourceUrl: senderBank.fundingSourceUrl,
+        destinationFundingSourceUrl: receiverBank.fundingSourceUrl,
+        amount: data.amount,
+      };
+      // create transfer
+      const transfer = await createTransfer(transferParams);
+
+      // create transfer transaction
+      if (transfer) {
+        const transaction = {
+          name: data.name,
+          amount: data.amount,
+          senderId: senderBank.userId.$id,
+          senderBankId: senderBank.$id,
+          receiverId: receiverBank.userId.$id,
+          receiverBankId: receiverBank.$id,
+          email: data.email,
+        };
+
+        // const newTransaction = await createTransaction(transaction);
+
+        // if (newTransaction) {
+        //   form.reset();
+        //   router.push("/");
+        // }
+      }
+    } catch (error) {
+      console.error("Submitting create transfer request failed: ", error);
     }
+
+    setIsLoading(false);
   };
 
   return (
-    <Select
-      defaultValue={selected.id}
-      onValueChange={(value) => handleBankChange(value)}
-    >
-      <SelectTrigger
-        className={`flex w-full gap-3 md:w-[300px] ${otherStyles}`}
-      >
-        <Image
-          src="icons/credit-card.svg"
-          width={20}
-          height={20}
-          alt="account"
-        />
-        <p className="line-clamp-1 w-full text-left">{selected.name}</p>
-      </SelectTrigger>
-      <SelectContent
-        className={`w-full md:w-[300px] ${otherStyles}`}
-        align="end"
-      >
-        <SelectGroup>
-          <SelectLabel className="py-2 font-normal text-gray-500">
-            Select a bank to display
-          </SelectLabel>
-          {accounts.map((account: Account) => (
-            <SelectItem
-              key={account.id}
-              value={account.appwriteItemId}
-              className="cursor-pointer border-t"
-            >
-              <div className="flex flex-col ">
-                <p className="text-16 font-medium">{account.name}</p>
-                <p className="text-14 font-medium text-blue-600">
-                  {formatAmount(account.currentBalance)}
-                </p>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(submit)} className="flex flex-col">
+        <FormField
+          control={form.control}
+          name="senderBank"
+          render={() => (
+            <FormItem className="border-t border-gray-200">
+              <div className="payment-transfer_form-item pb-6 pt-5">
+                <div className="payment-transfer_form-content">
+                  <FormLabel className="text-14 font-medium text-gray-700">
+                    送金元銀行を選択
+                  </FormLabel>
+                  <FormDescription className="text-12 font-normal text-gray-600">
+                    送金元の銀行口座を選択してください
+                  </FormDescription>
+                </div>
+                <div className="flex w-full flex-col">
+                  <FormControl>
+                    <BankDropdown
+                      accounts={accounts}
+                      setValue={form.setValue}
+                      otherStyles="!w-full"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-12 text-red-500" />
+                </div>
               </div>
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+            </FormItem>
+          )}
+        />
+  
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="border-t border-gray-200">
+              <div className="payment-transfer_form-item pb-6 pt-5">
+                <div className="payment-transfer_form-content">
+                  <FormLabel className="text-14 font-medium text-gray-700">
+                    送金メモ（任意）
+                  </FormLabel>
+                  <FormDescription className="text-12 font-normal text-gray-600">
+                    送金に関する追加情報や指示があれば入力してください
+                  </FormDescription>
+                </div>
+                <div className="flex w-full flex-col">
+                  <FormControl>
+                    <Textarea
+                      placeholder="ここに簡単なメモを入力してください"
+                      className="input-class"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-12 text-red-500" />
+                </div>
+              </div>
+            </FormItem>
+          )}
+        />
+  
+        <div className="payment-transfer_form-details">
+          <h2 className="text-18 font-semibold text-gray-900">
+            銀行口座の詳細
+          </h2>
+          <p className="text-16 font-normal text-gray-600">
+            受取人の銀行口座の詳細を入力してください
+          </p>
+        </div>
+  
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="border-t border-gray-200">
+              <div className="payment-transfer_form-item py-5">
+                <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                  受取人のメールアドレス
+                </FormLabel>
+                <div className="flex w-full flex-col">
+                  <FormControl>
+                    <Input
+                      placeholder="例： johndoe@gmail.com"
+                      className="input-class"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-12 text-red-500" />
+                </div>
+              </div>
+            </FormItem>
+          )}
+        />
+  
+        <FormField
+          control={form.control}
+          name="sharableId"
+          render={({ field }) => (
+            <FormItem className="border-t border-gray-200">
+              <div className="payment-transfer_form-item pb-5 pt-6">
+                <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                  受取人のPlaid共有可能ID
+                </FormLabel>
+                <div className="flex w-full flex-col">
+                  <FormControl>
+                    <Input
+                      placeholder="公開口座番号を入力してください"
+                      className="input-class"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-12 text-red-500" />
+                </div>
+              </div>
+            </FormItem>
+          )}
+        />
+  
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem className="border-y border-gray-200">
+              <div className="payment-transfer_form-item py-5">
+                <FormLabel className="text-14 w-full max-w-[280px] font-medium text-gray-700">
+                  金額
+                </FormLabel>
+                <div className="flex w-full flex-col">
+                  <FormControl>
+                    <Input
+                      placeholder="例： 5.00"
+                      className="input-class"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-12 text-red-500" />
+                </div>
+              </div>
+            </FormItem>
+          )}
+        />
+  
+        <div className="payment-transfer_btn-box">
+          <Button type="submit" className="payment-transfer_btn">
+            {isLoading ? (
+              <>
+                <Loader2 size={20} className="animate-spin" />   送金中...
+              </>
+            ) : (
+              "送金"
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 };
+
+export default PaymentTransferForm;
