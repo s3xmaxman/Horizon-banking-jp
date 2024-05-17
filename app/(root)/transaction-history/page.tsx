24bf7ebcf1caad5f1885dda35c6b4f99eeb67bad
s3xmaxman <badside3919@gmail.com>
2024-05-17 15:32:27 +0900

    Done

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,9 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
-import { Pagination } from '@/components/Pagination'
-import TransactionsTable from '@/components/TransactionsTable'
-import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
-import { getLoggedInUser } from '@/lib/actions/user.actions'
-import { formatAmount } from '@/lib/utils'
+import { Pagination } from '@/components/Pagination';
+import TransactionsTable from '@/components/TransactionsTable';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { formatAmount } from '@/lib/utils';
 import React from 'react'
 
 const TransactionHistory = async ({ searchParams: { id, page }}:SearchParamProps) => {
@@ -20,55 +20,55 @@ const TransactionHistory = async ({ searchParams: { id, page }}:SearchParamProps
 
   const account = await getAccount({ appwriteItemId })
 
-  const rowPerPage = 10;
-  const totalPages = Math.ceil(account?.data.transactions.length / rowPerPage);
 
-  const indexOfLastTransaction = currentPage * rowPerPage;
-  const indexOfFirstTransaction = indexOfLastTransaction - rowPerPage;
-  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
 
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(
+    indexOfFirstTransaction, indexOfLastTransaction
+  )
+  
   return (
-    <div className='transactions'>
-        <div className='transactions-header'>
-          <HeaderBox
-            title="取引履歴"
-            subtext="銀行の詳細と取引を確認できます。"
-          />
+    <div className="transactions">
+      <div className="transactions-header">
+        <HeaderBox 
+          title="取引履歴"
+          subtext="銀行の詳細と取引を確認してください。"
+        />
+      </div>
+
+      <div className="space-y-6">
+        <div className="transactions-account">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-18 font-bold text-white">{account?.data.name}</h2>
+            <p className="text-14 text-blue-25">
+              {account?.data.officialName}
+            </p>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● {account?.data.mask}
+            </p>
+          </div>
+          
+          <div className='transactions-account-balance'>
+            <p className="text-14">現在の口座残高</p>
+            <p className="text-24 text-center font-bold">{formatAmount(account?.data.currentBalance)}</p>
+          </div>
         </div>
 
-        <div className='space-y-6'>
-          <div className='transaction-account'>
-            <div className='flex flex-col gap-2'>
-              <h2 className=''>
-                {account?.data.name}
-              </h2>
-              <p className='text-14 text-blue-25'>
-                {account?.data.officialName}
-              </p>
-              <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                ●●●● ●●●● ●●●● {account?.data.mask}
-              </p>
-            </div> 
-
-            <div className='transactions-account-balance'>
-              <p className="text-14">現在の残高</p>
-              <p className="text-24 text-center font-bold">
-                {formatAmount(account?.data.currentBalance)}
-              </p>
-           </div>
-          </div>
-
-          <section className='flex w-full flex-col gap-6'>
-            <TransactionsTable 
-              transactions={currentTransactions}
-            />
+        <section className="flex w-full flex-col gap-6">
+          <TransactionsTable 
+            transactions={currentTransactions}
+          />
             {totalPages > 1 && (
               <div className="my-4 w-full">
                 <Pagination totalPages={totalPages} page={currentPage} />
               </div>
             )}
-          </section>
-        </div>
+        </section>
+      </div>
     </div>
   )
 }
